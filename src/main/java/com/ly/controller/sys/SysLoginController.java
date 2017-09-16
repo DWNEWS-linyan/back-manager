b61a7b3f23d3cf2aa732e5943bb3fbd975d4795a
linyan9204 <linyan1708@163.com>
2017-09-16 14:51:01 +0800

    update

--- src/main/java/com/ly/controller/sys/SysLoginController.java
+++ src/main/java/com/ly/controller/sys/SysLoginController.java
@@ -1,5 +1,19 @@
 package com.ly.controller.sys;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,9 +27,12 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.crazycake.shiro.RedisManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
 * @ClassName: SysLoginController
@@ -28,18 +45,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="sys")
 public class SysLoginController {
 
+	@Autowired
+	private RedisManager redisManager;
+	
+	
 	@RequestMapping(value="login")
 	public String login(String userName , String userPass , String remenber , HttpServletRequest request,HttpServletResponse response,Model model){
 		System.out.println("remenber:" + userName);
 		System.out.println("userPass:" + userPass);
 		System.out.println("remenber:" + remenber);
 //		SecurityUtils.getSubject().login(new UsernamePasswordToken(userName, userPass));
+		String url = "index";
+		try {
+			String verification = request.getParameter("verification"); //验证码
+			if (verification==null||verification.trim().equals("")) {
+				request.setAttribute("mes", "请输入验证码");
+				return url;
+			}
+			String verLower = verification.toLowerCase();
+			
+			String sessionId = request.getSession().getId();
+			
+			byte[] redisVeri = redisManager.get(sessionId.getBytes());
+			
+			if (redisVeri==null||redisVeri.length<1) {
+				request.setAttribute("mes", "验证码已过期");
+				return url;
+			}
+			String redisVerifi = new String(redisVeri).toLowerCase();
+			if (!verLower.equals(redisVerifi)) {
+				request.setAttribute("mes", "您输入的验证码不正确");
+				return url;
+			}
+		} catch (Exception e) {
+			request.setAttribute("mes", "您输入的用户名或密码不正确.");
+			return url;
+		}
 		
 		UsernamePasswordToken token = new UsernamePasswordToken(userName, userPass);
 		if (remenber != null && !remenber.trim().equals("")) {
 			token.setRememberMe(true);
 		}
-		String url = "index";
+		
 		System.out.println("为了验证登录用户而封装的token为" + ReflectionToStringBuilder.toString(token, ToStringStyle.MULTI_LINE_STYLE));
 		// 获取当前的Subject
 		Subject currentUser=SecurityUtils.getSubject();
@@ -78,6 +125,194 @@ public class SysLoginController {
 		return url;
 	}
 	
+	@RequestMapping("login/yanZhengMa")
+	public void yanZhengMa(HttpServletRequest request,HttpServletResponse response){
+		//设置不缓存图片  
+        response.setHeader("Pragma", "No-cache");  
+        response.setHeader("Cache-Control", "No-cache");  
+        response.setDateHeader("Expires", 0);  
+        //指定生成的响应图片,一定不能缺少这句话,否则错误.  
+        response.setContentType("image/jpeg");  
+        int width=86,height=40;     //指定生成验证码的宽度和高度  
+        BufferedImage image=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB); //创建BufferedImage对象,其作用相当于一图片  
+        Graphics g=image.getGraphics();     //创建Graphics对象,其作用相当于画笔  
+        Graphics2D g2d=(Graphics2D)g;       //创建Grapchics2D对象  
+        Random random=new Random();  
+        Font mfont=new Font("楷体",Font.BOLD,22); //定义字体样式  
+        g.setColor(getRandColor(200,250));  
+        g.fillRect(0, 0, width, height);    //绘制背景  
+        g.setFont(mfont);                   //设置字体  
+        g.setColor(getRandColor(180,200));  
+          
+        //绘制100条颜色和位置全部为随机产生的线条,该线条为2f  
+        for(int i=0;i<100;i++){  
+            int x=random.nextInt(width-1);  
+            int y=random.nextInt(height-1);  
+            int x1=random.nextInt(6)+1;  
+            int y1=random.nextInt(12)+1;  
+            BasicStroke bs=new BasicStroke(2f,BasicStroke.CAP_BUTT,BasicStroke.JOIN_BEVEL); //定制线条样式  
+            Line2D line=new Line2D.Double(x,y,x+x1,y+y1);  
+            g2d.setStroke(bs);  
+            g2d.draw(line);     //绘制直线  
+        }  
+        //输出由英文，数字，和中文随机组成的验证文字，具体的组合方式根据生成随机数确定。  
+        String sRand="";  
+        String ctmp="";  
+        int itmp=0;  
+        //制定输出的验证码为四位  
+        for(int i=0;i<4;i++){  
+//            switch(5){  
+            switch(random.nextInt(3)){  
+                case 1:     //生成A-Z的字母  
+                     itmp=random.nextInt(26)+65;  
+                     ctmp=String.valueOf((char)itmp);  
+                     break;  
+                case 2:     //生成汉字  
+                     String[] rBase={"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};   
+                     //生成第一位区码  
+                     int r1=random.nextInt(3)+11;  
+                     String str_r1=rBase[r1];  
+                     //生成第二位区码  
+                     int r2;  
+                     if(r1==13){  
+                         r2=random.nextInt(7);     
+                     }else{  
+                         r2=random.nextInt(16);  
+                     }  
+                     String str_r2=rBase[r2];  
+                     //生成第一位位码  
+                     int r3=random.nextInt(6)+10;  
+                     String str_r3=rBase[r3];  
+                     //生成第二位位码  
+                     int r4;  
+                     if(r3==10){  
+                         r4=random.nextInt(15)+1;  
+                     }else if(r3==15){  
+                         r4=random.nextInt(15);  
+                     }else{  
+                         r4=random.nextInt(16);  
+                     }  
+                     String str_r4=rBase[r4];  
+                     //将生成的机内码转换为汉字  
+                     byte[] bytes=new byte[2];  
+                     //将生成的区码保存到字节数组的第一个元素中  
+                     String str_12=str_r1+str_r2;  
+                     int tempLow=Integer.parseInt(str_12, 16);  
+                     bytes[0]=(byte) tempLow;  
+                     //将生成的位码保存到字节数组的第二个元素中  
+                     String str_34=str_r3+str_r4;  
+                     int tempHigh=Integer.parseInt(str_34, 16);  
+                     bytes[1]=(byte)tempHigh;  
+						try {
+							ctmp=new String(bytes,"GBK");
+						} catch (UnsupportedEncodingException e) {
+							ctmp = "了";
+							e.printStackTrace();
+						}  
+                     break;  
+                default:  
+                     itmp=random.nextInt(10)+48;  
+                     ctmp=String.valueOf((char)itmp);  
+                     break;  
+            }  
+            sRand+=ctmp;  
+            Color color=new Color(20+random.nextInt(110),20+random.nextInt(110),random.nextInt(110));  
+            g.setColor(color);  
+            //将生成的随机数进行随机缩放并旋转制定角度 PS.建议不要对文字进行缩放与旋转,因为这样图片可能不正常显示  
+            /*将文字旋转制定角度*/  
+            Graphics2D g2d_word=(Graphics2D)g;  
+            AffineTransform trans=new AffineTransform();
+            int du = random.nextInt(10);
+            trans.rotate((du+30)*3.14/180,15*i+8,7);  
+            /*缩放文字*/  
+            float scaleSize=random.nextFloat()+0.8f;  
+            if(scaleSize>1f) scaleSize=1f;  
+            trans.scale(scaleSize, scaleSize);  
+            g2d_word.setTransform(trans);
+            if (i==0) {
+            	g.drawString(ctmp, 15*i+10, 20); 
+			}else {
+				g.drawString(ctmp, 15*i+20, 20); 
+			}
+             
+        }  
+        redisManager.set(request.getSession().getId().getBytes(), sRand.getBytes(), 3000);
+        g.dispose();    //释放g所占用的系统资源  
+        try {
+        	 ImageIO.write(image,"JPEG",response.getOutputStream()); //输出图片
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*该方法主要作用是获得随机生成的颜色*/   
+    public Color getRandColor(int s,int e){  
+        Random random=new Random ();  
+        if(s>255) s=255;  
+        if(e>255) e=255;  
+        int r,g,b;  
+        r=s+random.nextInt(e-s);    //随机生成RGB颜色中的r值  
+        g=s+random.nextInt(e-s);    //随机生成RGB颜色中的g值  
+        b=s+random.nextInt(e-s);    //随机生成RGB颜色中的b值  
+        return new Color(r,g,b);  
+    }
+	
+	@RequestMapping(value = "keyDown")
+	@ResponseBody
+	public Object keyDown(String verification,HttpServletRequest request ,HttpServletResponse response){
+		Map<String, Object> map = new HashMap<String, Object>();
+		if (verification==null||verification.trim().equals("")) {
+			map.put("code", 1);
+			map.put("mes", "您输入的验证码为空");
+		}else {
+			String sesseionId = null ;
+			try {
+				sesseionId = request.getSession().getId();
+				byte[] redisVeri = redisManager.get(sesseionId.getBytes());
+				if (redisVeri == null || redisVeri.length<1) {
+					map.put("mes", "验证码已过期，请刷新");
+					map.put("code", 3);
+				}else {
+					String redisVerification = new String(redisVeri).toLowerCase();
+
+					if (verification.trim().length()>redisVerification.length()) {
+						map.put("code", 6);
+						map.put("mes", "位数过多");
+					}else {
+						if (verification.trim().length()==redisVerification.length()) {
+							String verifiLower = verification.trim().toLowerCase();
+							if (verifiLower.equals(redisVerification)) {
+								map.put("code", 0);
+								map.put("mes", "");
+							}else {
+								map.put("code", 7);
+								map.put("mes", "验证码不正确");
+							}
+						}else {
+							String verifiLower = verification.trim().toLowerCase();
+							
+							String verifiLength = redisVerification.substring(0,verification.trim().length());
+							
+							if (verifiLower.trim().equals(verifiLength)) {
+								map.put("code", 5);
+								map.put("mes", "部分相同");
+							}else {
+								map.put("code", 4);
+								map.put("mes", "验证码不正确.");
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				map.put("code", 2);
+				map.put("mes", "出错了 ");
+			}
+		}
+		return map;
+	}
+    
+    
+    
 	@RequestMapping(value = "index")
 	public String index(String mes,Model model){
 		SecurityUtils.getSubject().logout();
--- src/main/java/com/ly/utils/SendMail.java
+++ src/main/java/com/ly/utils/SendMail.java
@@ -1,84 +1,237 @@
 package com.ly.utils;
 
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message.RecipientType;
-import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class SendMail {
-	@SuppressWarnings("static-access")
-	public static void sendMessage(String smtpHost, String from,
-			String fromUserPassword, String to, String subject,
-			String messageText, String messageType) throws MessagingException {
-		// 第一步：配置javax.mail.Session对象
-		System.out.println("为" + smtpHost + "配置mail session对象");
+	
+	/**
+	 * @Title: sendMessage
+	 * @Description: 发送邮件
+	 * @param isAuth   是否验证  true  是  
+	 * @param smtpHost  发送 服务器地址
+	 * @param smtpPort 发送邮件需要的端口
+	 * @param isSSL    是否 使用 SSL 协议证书   
+	 * @param fromUser 发件人地址
+	 * @param fromUserPassword  发件人邮箱密码 
+	 * @param toUsers  发件人地址
+	 * @param ccUsers  抄送人 地址
+	 * @param bccUsers  密送人 地址
+	 * @param title    邮件标题
+	 * @param messageText   邮件  内容
+	 * @param messageType   邮件 内容的 类型
+	 * @param files			附件
+	 * @throws Exception    
+	 */
+	public static void sendMessage(boolean isAuth,String smtpHost,String smtpPort,boolean isSSL, String fromUser,
+			String fromUserPassword, List<String> toUsers,List<String> ccUsers,List<String> bccUsers, String title,
+			String messageText, String messageType,List<File> files) throws Exception {
+		/** 参数验证   开始 */
+		if(StringUtils.isEmpty(smtpHost)){
+			throw new NullPointerException("smtpHost is Required");
+		}
+		if(StringUtils.isEmpty(smtpPort)){
+			throw new NullPointerException("smtpPort is Required");
+		}
+		if (isAuth) {
+			if(StringUtils.isEmpty(fromUser)){
+				throw new NullPointerException("fromUser is Required");
+			}
+			if(StringUtils.isEmpty(fromUserPassword)){
+				throw new NullPointerException("fromUserPassword is Required");
+			}
+		}
+		if(StringUtils.isEmpty(title)){
+			throw new NullPointerException("title is Required");
+		}
+		if(StringUtils.isEmpty(messageText)){
+			throw new NullPointerException("messageText is Required");
+		}
+		if (StringUtils.isEmpty(messageType)) {
+			messageType = "text/html;charset=gb2312";
+		}
+		if(toUsers==null||toUsers.size()<1){
+			throw new NullPointerException("toUsers is Required");
+		}
+		if (ccUsers==null||ccUsers.size()<1) {
+			ccUsers = new ArrayList<String>();
+		}
+		if (bccUsers==null||bccUsers.size()<1) {
+			bccUsers = new ArrayList<String>();
+		}
+		if (files==null||files.size()<1) {
+			files = new ArrayList<File>();
+		}
+		/** 参数验证   结束  */
 		
-		
-		Properties props = new Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.starttls.enable","true");//使用 STARTTLS安全连接
-//		props.put("mail.smtp.port", "25");			 //google使用465或587端口
-		props.put("mail.smtp.port", "465");			 //google使用或587端口
-//		props.put("smtp.global-mail.cn", "25");			 //google使用465或587端口
-		props.put("mail.smtp.auth", "true");		// 使用验证
+		//Session对象 所需要的参数
+		Properties props = System.getProperties();
+		//发送 服务器地址 
+		props.setProperty("mail.smtp.host", smtpHost);
+		//发送邮件需要的端口  这个端口 是和 发送服务器 地址 相关联的    不用的服务器地址 端口号是不同的
+		props.setProperty("mail.smtp.port", smtpPort);
+		//验证
+		props.put("mail.smtp.auth", isAuth ? "true":"false");
+		//是否 使用  STARTTLS 加密传输
+		props.setProperty("mail.smtp.starttls.enable","true");
+		//是否 使用 SSL 协议证书   
+		props.setProperty("mail.smtp.ssl.enable", isSSL ? "true":"false");
+		//这是看 Properties 日志的  真正上线后 可以 不用设置   默认 为 false
 		props.put("mail.debug", "true");
-		props.setProperty("mail.smtp.ssl.enable", "true");
-		Session mailSession = Session.getInstance(props,new MyAuthenticator(from,fromUserPassword));
+		//Session对象
+		Session mailSession = null;
+		if (isAuth) {
+			mailSession = Session.getInstance(props,new Authenticator() {
+				//验证    也可以写一个类  继承 Authenticator 重写  getPasswordAuthentication 
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(fromUser,fromUserPassword);
+				}
+			});
+		}else {
+			mailSession = Session.getInstance(props);
+		}
+		//这是看 Session 日志的  真正上线后 可以 不用设置   默认 为 false
 		mailSession.setDebug(true);
-		// 第二步：编写消息
-		System.out.println("编写消息from——to:" + from + "——" + to);
-
-		InternetAddress fromAddress = new InternetAddress(from);
-		InternetAddress toAddress = new InternetAddress(to);
-
+		//得到 message 对象
 		MimeMessage message = new MimeMessage(mailSession);
 
+		//发件人地址
+		InternetAddress fromAddress = new InternetAddress(fromUser);
+		// 设置发件人
 		message.setFrom(fromAddress);
-		message.addRecipient(RecipientType.TO, toAddress);
+		// 这里有几种方法
+		//直接循环 一个一个 添加 到message中  
+		{
+			for (String toUser : toUsers) {
+				InternetAddress toAddress = new InternetAddress(toUser);
+				//用 addRecipient 方法  设置 收件人
+				message.addRecipient(RecipientType.TO, toAddress);
+			}
+		}
+		/**
+		//组装成 Address[] 数组    然后 在添加到 mesage中
+		{
+			Address[] toUsersAddresses = new Address[toUsers.size()];
+			for (int i = 0; i < toUsers.size(); i++) {
+				String toUser = toUsers.get(i);
+				InternetAddress toAddress = new InternetAddress(toUser);
+				toUsersAddresses[i] = toAddress;
+			}
+			//用 addRecipients 方法  
+			message.addRecipients(RecipientType.TO,toUsersAddresses);
+		}
 
+		//还可以直接用string 添加 到 mesage中
+		{
+			//这里 主要是  拼成  xx@123.com,xx@123.com 这种形式    用英文逗号（,）分隔的形式  
+			//也可以用其他方法
+			StringBuffer toUsersAddresses  = new StringBuffer();
+			for (String toUser : toUsers) {
+				toUsersAddresses.append(toUser+",");
+			}
+			String toUserAddresses = toUsersAddresses.substring(0, toUsersAddresses.length()-1);
+			//用 addRecipients 方法 .  
+			//这个方法 在源码中 同样 也是转换成了  InternetAddress[] 数组  .  InternetAddress 类 是Address 的子类
+			//InternetAddress.parse(addresses)   参数是string 返回 InternetAddress[] 数组  .
+			message.addRecipients(RecipientType.TO,toUserAddresses);
+		}
+		*/
+		for (String ccUser : ccUsers) {
+			InternetAddress ccAddress = new InternetAddress(ccUser);
+			//设置 抄送人 地址
+			message.addRecipient(RecipientType.CC, ccAddress);
+		}
+		for (String bccUser : bccUsers) {
+			InternetAddress bccAddress = new InternetAddress(bccUser);
+			//设置 密送人 地址
+			message.addRecipient(RecipientType.BCC, bccAddress);
+		}
+		//设置 发送 时间
 		message.setSentDate(Calendar.getInstance().getTime());
-		message.setSubject(subject);
-		message.setContent(messageText, messageType);
-
-		// 第三步：发送消息
-		Transport transport = mailSession.getTransport("smtp");
-		transport.connect(smtpHost,from, fromUserPassword);
-		transport.send(message, message.getRecipients(RecipientType.TO));
-		System.out.println("message yes");
+		//设置标题
+		message.setSubject(title);
+		//设置 邮件内容
+		//用  Multipart 设置参数
+		Multipart multipart = new MimeMultipart();
+		//循环 文件  分别 添加 到 Multipart 中
+		for (File file : files) {
+			//设置  内容
+			MimeBodyPart mimeBodyPart = new MimeBodyPart();
+			//设置 文件 
+			DataSource dataSource = new FileDataSource(file);
+			// 设置 内容  头
+			DataHandler dataHandler = new DataHandler(dataSource);
+			mimeBodyPart.setDataHandler(dataHandler);
+			//  MimeUtility.encodeText()   这个发放 是为了方式乱码的
+			mimeBodyPart.setFileName(MimeUtility.encodeText(dataHandler.getName()));
+			multipart.addBodyPart(mimeBodyPart);
+		}
+		//设置  消息体
+		BodyPart part = new MimeBodyPart();
+		part.setContent(messageText, messageType);
+		multipart.addBodyPart(part);
+		// 将  消息体 和 附件 放到  message 消息 中
+		message.setContent(multipart);
+		//发送消息
+		Transport.send(message);
 	}
 
 	public static void main(String[] args) {
 		try {
-			SendMail.sendMessage("smtp.gmail.com", "linyan@dwnews.com",
-					"li8..", "wangkai@dwnews.com", "nihao",
-					"---------------wrwe-----------",
-					"text/html;charset=gb2312");
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
+			List<String> toUsers = new ArrayList<String>();
+			toUsers.add("linyan@caixun.com");
+//			toUsers.add("linyan@caixun.com");
+			List<String> ccUsers = new ArrayList<String>();
+			ccUsers.add("linyan9248@gmail.com");
+			List<File> files = new ArrayList<File>();
+			files.add(new File("C:\\Users\\linyan\\Desktop\\APP管理后台PHP端接口.doc"));
+//			FileInputStream inputStream = new FileInputStream(new File("C:\\Users\\linyan\\Desktop\\APP管理后台PHP端接口.doc"));
+//			SendMail.sendMessage("smtp.gmail.com", "465", true, "linyan@dwnews.com", "linyan9248..", toUsers, ccUsers, "agg", "ggggggggggggg", "text/html;charset=gb2312",inputStream);
+			SendMail.sendMessage(true,"smtp.global-mail.cn", "25", false, "linyan@caixun.com", "ly123456..", toUsers,null, null, "agg", "gggggg是打发ggggggg",null,null);
+//			SendMail.sendMessage("smtp.gmail.com", "linyan@dwnews.com",
+////					SendMail.sendMessage("smtp.gmail.com", "linyan9248@gmail.com",
+//					"linyan9248..", "linyan@caixun.com", "nihao",
+//					"---------------wrwe-----------",
+//					"text/html;charset=gb2312");
+//			SendMail.sendMessage("smtp.global-mail.cn", "linyan@caixun.com",
+//					"ly123456..", "linyan@dwnews.com", "nihao",
+//					"---------------wrwe-----------",
+//					"text/html;charset=gb2312");
+			String lajlkString = "linyan@caixun.com";
+			
+			
+			InternetAddress[] gg = InternetAddress.parse(lajlkString);
+			
+			for (InternetAddress address : gg) {
+				String a = address.getAddress();
+				System.out.println(a);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-}
-class MyAuthenticator extends Authenticator{
-	String userName="";
-	String password="";
-	public MyAuthenticator(){
-		
-	}
-	public MyAuthenticator(String userName,String password){
-		this.userName=userName;
-		this.password=password;
-	}
-	 protected PasswordAuthentication getPasswordAuthentication(){   
-		return new PasswordAuthentication(userName, password);   
-	 } 
 }
